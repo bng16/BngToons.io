078ae582169e90ff9c50d828697a0c3cbb560818
Biswajit Nag <95161306+bng16@users.noreply.github.com>
2023-07-28 05:06:02 +0530

    Add files via upload
    uploaded all the files!

---
+++ script.js
@@ -0,0 +1,245 @@
+var tl2=gsap.timeline({scrollTrigger:{
+    trigger:"#page2",
+    start:"0% 80%",
+    end:"8% 15%",
+    scrub:5,
+    duration:2,
+    // markers:true,
+}})
+
+tl2.to("#main",{
+    backgroundColor:"#E75A48"
+})
+
+
+var tl3=gsap.timeline({scrollTrigger:{
+    trigger:"#page3",
+    start:"0% 80%",
+    end:"8% 15%",
+    scrub:5,
+    duration:2,
+    // markers:true,
+}})
+
+tl3.to("#main",{
+    backgroundColor:"#AA9E92"
+})
+
+
+
+var tl4=gsap.timeline({scrollTrigger:{
+    trigger:"#page4",
+    start:"0% 80%",
+    end:"8% 15%",
+    scrub:5,
+    duration:2,
+    // markers:true,
+}})
+
+tl4.to("#main",{
+    backgroundColor:"#E8C57B"
+})
+
+
+var tl5=gsap.timeline({scrollTrigger:{
+    trigger:"#page5",
+    start:"0% 80%",
+    end:"8% 15%",
+    scrub:5,
+    duration:2,
+    // markers:true,
+}})
+
+tl5.to("#main",{
+    backgroundColor:"#C2D562"
+})
+
+
+
+var tl6=gsap.timeline({scrollTrigger:{
+    trigger:"#page6",
+    start:"0% 80%",
+    end:"8% 15%",
+    scrub:5,
+    duration:2,
+    // markers:true,
+}})
+
+tl6.to("#main",{
+    backgroundColor:"#FDEB3D"
+})
+
+var tl7=gsap.timeline({scrollTrigger:{
+    trigger:"#page7",
+    start:"0% 80%",
+    end:"8% 15%",
+    scrub:5,
+    duration:2,
+    // markers:true,
+}})
+
+tl7.to("#main",{
+    backgroundColor:"#D2ECF6"
+})
+
+
+var tl8=gsap.timeline({scrollTrigger:{
+    trigger:"#page8",
+    start:"0% 80%",
+    end:"8% 15%",
+    scrub:5,
+    duration:2,
+    // markers:true,
+}})
+
+tl8.to("#main",{
+    backgroundColor:"#72BA7F"
+})
+
+
+
+var tl9=gsap.timeline({scrollTrigger:{
+    trigger:"#page9",
+    start:"0% 80%",
+    end:"8% 15%",
+    scrub:5,
+    duration:2,
+    // markers:true,
+}})
+
+tl9.to("#main",{
+    backgroundColor:"#68CBC6"
+})
+
+
+var tl10=gsap.timeline({scrollTrigger:{
+    trigger:"#page10",
+    start:"0% 80%",
+    end:"8% 15%",
+    scrub:5,
+    duration:2,
+    // markers:true,
+}})
+
+tl10.to("#main",{
+    backgroundColor:"#EB80DE"
+})
+
+
+var tl11=gsap.timeline({scrollTrigger:{
+    trigger:"#page11",
+    start:"0% 80%",
+    end:"8% 15%",
+    scrub:5,
+    duration:2,
+    // markers:true,
+}})
+
+tl11.to("#main",{
+    backgroundColor:"#B8CFB2"
+})
+
+
+
+var tl12=gsap.timeline({scrollTrigger:{
+    trigger:"#page12",
+    start:"0% 80%",
+    end:"8% 15%",
+    scrub:5,
+    duration:2,
+    // markers:true,
+}})
+
+tl12.to("#main",{
+    backgroundColor:"#ECBD7D"
+})
+
+
+
+var tl13=gsap.timeline({scrollTrigger:{
+    trigger:"#page13",
+    start:"0% 80%",
+    end:"8% 15%",
+    scrub:5,
+    duration:2,
+    // markers:true,
+}})
+
+tl13.to("#main",{
+    backgroundColor:"#A94546"
+})
+
+
+
+var tl14=gsap.timeline({scrollTrigger:{
+    trigger:"#page14",
+    start:"0% 80%",
+    end:"8% 15%",
+    scrub:5,
+    duration:2,
+    // markers:true,
+}})
+
+tl14.to("#main",{
+    backgroundColor:"#547BA2"
+})
+
+
+
+var tl15=gsap.timeline({scrollTrigger:{
+    trigger:"#page15",
+    start:"0% 80%",
+    end:"8% 15%",
+    scrub:5,
+    duration:2,
+    // markers:true,
+}})
+
+tl15.to("#main",{
+    backgroundColor:"#D7785C"
+})
+
+
+var tl16=gsap.timeline({scrollTrigger:{
+    trigger:"#page16",
+    start:"0% 80%",
+    end:"8% 15%",
+    scrub:5,
+    duration:2,
+    // markers:true,
+}})
+
+tl16.to("#main",{
+    backgroundColor:"#78A761"
+})
+
+
+
+var tl17=gsap.timeline({scrollTrigger:{
+    trigger:"#page17",
+    start:"0% 80%",
+    end:"8% 15%",
+    scrub:5,
+    duration:2,
+    // markers:true,
+}})
+
+tl17.to("#main",{
+    backgroundColor:"#DCDDD7"
+})
+
+
+
+var tl18=gsap.timeline({scrollTrigger:{
+    trigger:"#page18",
+    start:"0% 80%",
+    end:"8% 15%",
+    scrub:5,
+    duration:2,
+    // markers:true,
+}})
+
+tl18.to("#main",{
+    backgroundColor:"#F8942E"
+})
+
